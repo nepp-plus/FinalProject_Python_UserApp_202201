ef41ebb450e25cf735df5a784bcef877043df091
nepp.plus <nepp.plus@gmail.com>
2022-01-21 13:16:08 +0900

    회원가입 화면 작업

--- app/src/main/java/com/neppplus/finalproject_python_userapp_202201/LoginActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_python_userapp_202201/LoginActivity.kt
@@ -1,17 +1,30 @@
 package com.neppplus.finalproject_python_userapp_202201
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_python_userapp_202201.databinding.ActivityLoginBinding
 
 class LoginActivity : BaseActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+
+        binding.btnSignUp.setOnClickListener {
+
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
     }
 
--- app/src/main/java/com/neppplus/finalproject_python_userapp_202201/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_python_userapp_202201/SignUpActivity.kt
@@ -2,11 +2,15 @@ package com.neppplus.finalproject_python_userapp_202201
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.finalproject_python_userapp_202201.databinding.ActivitySignUpBinding
 
 class SignUpActivity : BaseActivity() {
+    lateinit var binding : ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
         setupEvents()
         setValues()
     }
