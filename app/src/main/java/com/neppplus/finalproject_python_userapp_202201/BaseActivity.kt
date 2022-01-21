5f3f85ffb8177dcfca1a44c1f36b17c20dad744b
nepp.plus <nepp.plus@gmail.com>
2022-01-21 14:41:40 +0900

    커스텀액션바

--- app/src/main/java/com/neppplus/finalproject_python_userapp_202201/BaseActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_python_userapp_202201/BaseActivity.kt
@@ -3,7 +3,10 @@ package com.neppplus.finalproject_python_userapp_202201
 import android.content.Context
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -12,9 +15,24 @@ abstract class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
+
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
     }
 
     abstract fun setupEvents()
     abstract fun setValues()
+
+    private fun setCustomActionBar() {
+
+        val defaultActionBar = supportActionBar!!
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.custom_action_bar)
+        val toolBar =  defaultActionBar.customView.parent as Toolbar
+        toolBar.setContentInsetsAbsolute(0, 0)
+
+
+    }
 
 }
