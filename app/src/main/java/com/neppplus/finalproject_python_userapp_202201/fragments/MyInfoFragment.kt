8dc4e89cf968e1696bf125e03429ac8c0ef250b8
nepp.plus <nepp.plus@gmail.com>
2022-01-24 11:56:52 +0900

    로그아웃 처리

--- app/src/main/java/com/neppplus/finalproject_python_userapp_202201/fragments/MyInfoFragment.kt
+++ app/src/main/java/com/neppplus/finalproject_python_userapp_202201/fragments/MyInfoFragment.kt
@@ -1,14 +1,19 @@
 package com.neppplus.finalproject_python_userapp_202201.fragments
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.neppplus.finalproject_python_userapp_202201.R
+import com.neppplus.finalproject_python_userapp_202201.SplashActivity
 import com.neppplus.finalproject_python_userapp_202201.databinding.FragmentHomeBinding
 import com.neppplus.finalproject_python_userapp_202201.databinding.FragmentMyInfoBinding
+import com.neppplus.finalproject_python_userapp_202201.utils.ContextUtil
 
 class MyInfoFragment : BaseFragment() {
 
@@ -30,6 +35,25 @@ class MyInfoFragment : BaseFragment() {
     }
 
     override fun setupEvents() {
+
+        binding.btnLogout.setOnClickListener {
+
+            val alert = AlertDialog.Builder(mContext)
+                .setTitle("로그아웃")
+                .setMessage("정말 로그아웃 하시겠습니까?")
+                .setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+                    ContextUtil.setToken(mContext, "")
+
+                    val myIntent = Intent(mContext, SplashActivity::class.java)
+                    myIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(myIntent)
+
+                })
+                .setNegativeButton("취소", null)
+
+            alert.show()
+
+        }
 
     }
 
