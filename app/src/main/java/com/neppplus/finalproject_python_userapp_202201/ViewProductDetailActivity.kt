bb54740fa310fa8ab73e2cc9032ad16ecd004406
nepp.plus <nepp.plus@gmail.com>
2022-01-24 16:44:09 +0900

    수량조절작업

--- app/src/main/java/com/neppplus/finalproject_python_userapp_202201/ViewProductDetailActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_python_userapp_202201/ViewProductDetailActivity.kt
@@ -1,16 +1,21 @@
 package com.neppplus.finalproject_python_userapp_202201
 
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.finalproject_python_userapp_202201.databinding.ActivityViewProductDetailBinding
 import com.neppplus.finalproject_python_userapp_202201.models.ProductData
+import com.neppplus.finalproject_python_userapp_202201.utils.WonFormatUtil
 
 class ViewProductDetailActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewProductDetailBinding
 
     lateinit var mProduct : ProductData
+
+    var buyQuantity = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,30 @@ class ViewProductDetailActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.btnPlus.setOnClickListener {
+            buyQuantity += 1
+            setQuantityAndPriceTxt()
+        }
+
+        binding.btnMinus.setOnClickListener {
+            if (buyQuantity > 1) {
+                buyQuantity -= 1
+                setQuantityAndPriceTxt()
+            }
+        }
+
+    }
+
+    private fun setQuantityAndPriceTxt() {
+
+        binding.txtOriginalPrice.text = WonFormatUtil.getWonFormat(mProduct.original_price * buyQuantity)
+        binding.txtOriginalPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG or binding.txtOriginalPrice.paintFlags
+        binding.txtSalePrice.text = WonFormatUtil.getWonFormat(mProduct.sale_price * buyQuantity)
+        val salePercent = (((mProduct.original_price - mProduct.sale_price).toDouble() / mProduct.original_price.toDouble()) * 100).toInt()
+
+        binding.txtSalePercent.text = "${salePercent}%"
+
+        binding.txtQuantity.text = buyQuantity.toString()
     }
 
     override fun setValues() {
@@ -29,6 +58,8 @@ class ViewProductDetailActivity : BaseActivity() {
         setTitle("상품 상세정보")
 
         binding.txtProductName.text = mProduct.name
+
+        setQuantityAndPriceTxt()
 
     }
 }
