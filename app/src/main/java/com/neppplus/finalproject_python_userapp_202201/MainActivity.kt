7682f3c69cd0b6373471a43ede92cded5d179b43
nepp.plus <nepp.plus@gmail.com>
2022-01-24 10:32:52 +0900

    뷰페이져 구조까지

--- app/src/main/java/com/neppplus/finalproject_python_userapp_202201/MainActivity.kt
+++ app/src/main/java/com/neppplus/finalproject_python_userapp_202201/MainActivity.kt
@@ -2,8 +2,15 @@ package com.neppplus.finalproject_python_userapp_202201
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.neppplus.finalproject_python_userapp_202201.databinding.ActivityMainBinding
+import com.neppplus.finalproject_python_userapp_202201.fragments.HomeFragment
 
 class MainActivity : BaseActivity() {
 
@@ -21,6 +28,60 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+        binding.mainViewPager.offscreenPageLimit = 4
+        binding.mainViewPager.adapter = PagerAdapter(supportFragmentManager, lifecycle)
+        binding.mainViewPager.registerOnPageChangeCallback(PageChangeCallback())
+        binding.mainBottomNav.setOnItemSelectedListener { navigationSelected(it) }
 
+    }
+
+    private inner class PagerAdapter(fm: FragmentManager, lc: Lifecycle) :
+        FragmentStateAdapter(fm, lc) {
+        override fun getItemCount() = 4
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> HomeFragment()
+                1 -> HomeFragment()
+                2 -> HomeFragment()
+                3 -> HomeFragment()
+                else -> error("no such position: $position")
+            }
+        }
+    }
+
+    private fun navigationSelected(item: MenuItem): Boolean {
+        val checked = item.setChecked(true)
+        when (checked.itemId) {
+            R.id.nav_home -> {
+                binding.mainViewPager.currentItem = 0
+                return true
+            }
+            R.id.nav_category -> {
+                binding.mainViewPager.currentItem = 1
+                return true
+            }
+            R.id.nav_cart -> {
+                binding.mainViewPager.currentItem = 2
+                return true
+            }
+            R.id.nav_my_info -> {
+                binding.mainViewPager.currentItem = 3
+                return true
+            }
+        }
+        return false
+    }
+
+    private inner class PageChangeCallback: ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            binding.mainBottomNav.selectedItemId = when (position) {
+                0 -> R.id.nav_home
+                1 -> R.id.nav_category
+                2 -> R.id.nav_cart
+                3 -> R.id.nav_my_info
+                else -> error("no such position: $position")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/neppplus/finalproject_python_userapp_202201/fragments/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.neppplus.finalproject_python_userapp_202201.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.neppplus.finalproject_python_userapp_202201.R
+import com.neppplus.finalproject_python_userapp_202201.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
